cc9490d40657ed9fe3284b5cd87b83a60834fd7f
Duvan Alexis Valencia Munca <d.alexis.valencia@gmail.com>
2026-07-08 08:51:14 -0500

    Add react route and test some routes

---
+++ src/routes.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import Login from './authentication/Login';
+import Register from './authentication/Register'
+import NotFound from './errors/NotFound';
+import App from './App';
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+                errorElement: <NotFound />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+                errorElement: <NotFound />,
+            }
+        ]
+    },
+]);
